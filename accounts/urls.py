7fc714146682934b1bb74008257cfbff6ab19128
hwangtate <ghkdxodud10253504@gmail.com>
2024-08-31 15:53:51 +0900

    Style: 주석처리

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("accounts/signup/", view=views.account_signup_view),
+    # path("accounts/signup/", view=views.account_signup_view),
     path("accounts/", include("allauth.urls")),
 ]
